fe71eb85f676bb4a4eebeaa1e775721a55c4cc0c
Hyeongcheol-An <anh1026@kaist.ac.kr>
2019-08-16 14:55:21 +0000

    Modified the node.sh

--- node.sh
+++ node.sh
@@ -1,2 +1,2 @@
 sudo apt-get install build-essential libssl-dev
-wget -qO- https://raw.githubsercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash

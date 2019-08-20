0265978d24dbefc59fb1910683a3afa6d303fd24
Hyeongcheol-An <anh1026@kaist.ac.kr>
2019-08-20 11:41:27 +0000

    Add the new file

---
+++ vm/org1-1/fabric-samples/first-network/myscript/case1/run.sh
@@ -0,0 +1,16 @@
+#run script
+rm -rf crypto-config
+
+../../../bin/cryptogen generate --config=./crypto-config.yaml
+
+rm -rf channel-artifacts
+mkdir channel-artifacts
+export FABRIC_CFG_PATH=$PWD
+
+../../../bin/configtxgen -profile TwoOrgsOrdererGenesis -channelID byfn -outputBlock ./channel-artifacts/genesis.block
+
+../../../bin/configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel1.tx -channelID channel1
+
+../../../bin/configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID channel1 -asOrg Org1MSP
+../../../bin/configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID channel1 -asOrg Org2MSP
+
---
+++ vm/org1-1/fabric-samples/first-network/myscript/case1/scp.sh
@@ -0,0 +1,8 @@
+sudo scp -r -i ./tmp.pem /home/an/GCP_Hyperledger/vm/org1-1/fabric-samples/first-network/myscript/case1/channel-artifacts/* an@35.232.221.188:/home/an/GCP_Hyperledger/vm/org2-1/fabric-samples/first-network/myscript/case1/channel-artifacts/
+
+sudo scp -r -i ./tmp.pem /home/an/GCP_Hyperledger/vm/org1-1/fabric-samples/first-network/myscript/case1/channel-artifacts/* an@35.232.167.182:/home/an/GCP_Hyperledger/vm/orderer1/fabric-samples/first-network/myscript/case1/channel-artifacts/
+
+sudo scp -r -i ./tmp.pem /home/an/GCP_Hyperledger/vm/org1-1/fabric-samples/first-network/myscript/case1/crypto-config/* an@35.232.221.188:/home/an/GCP_Hyperledger/vm/org2-1/fabric-samples/first-network/myscript/case1/crypto-config/
+
+sudo scp -r -i ./tmp.pem /home/an/GCP_Hyperledger/vm/org1-1/fabric-samples/first-network/myscript/case1/crypto-config/* an@35.232.167.182:/home/an/GCP_Hyperledger/vm/orderer1/fabric-samples/first-network/myscript/case1/crypto-config/
+
